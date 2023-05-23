f73e97100c9b9fa21082838de4886538bf8bb046
olive <developpeurolive.ios@gmail.com>
2023-05-23 17:43:41 +0200

    init project and remove storyboard

---
+++ ChatAppMessengerUIKit/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatAppMessengerUIKit
+//
+//  Created by olivier geiger on 23/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
